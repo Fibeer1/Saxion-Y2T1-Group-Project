894a3ab22fb720c911386783e5446eb8e00676fe
Fibeer1 <fibeer67@gmail.com>
2024-09-24 11:13:49 +0200

    reverted foventity script changes

--- Assets/Scripts/FOVEntity.cs
+++ Assets/Scripts/FOVEntity.cs
@@ -7,25 +7,15 @@ public class FOVEntity : MonoBehaviour
     private Renderer[] meshRenderers;
     public bool isBeingSeen = false;
     private LayerMask originalLayer;
-    [SerializeField] private Material transparentMaterial;
-    private List<Material> rendererMaterials = new List<Material>();
-    private bool isFading;
-    private bool isVisible;
 
     private void Start()
     {
         meshRenderers = GetComponentsInChildren<Renderer>();
-        foreach (var renderer in meshRenderers)
-        {
-            rendererMaterials.Add(renderer.material);
-        }
         originalLayer = gameObject.layer;
         isBeingSeen = false;
-        isVisible = false;
-        for (int i = 0; i < meshRenderers.Length; i++)
+        foreach (var renderer in meshRenderers)
         {
-            meshRenderers[i].enabled = false;
-            rendererMaterials[i] = meshRenderers[i].material;
+            renderer.enabled = false;
             gameObject.layer = 2;
         }
     }
@@ -37,60 +27,21 @@ public class FOVEntity : MonoBehaviour
 
     private void HandleBeingSeen()
     {
-        if (isBeingSeen && !isVisible)
-        {
-            StartCoroutine(FadeOnAppearing(true));
-        }
-        else if (isVisible)
-        {
-            StartCoroutine(FadeOnAppearing(false));
-        }
-    }
-
-    public IEnumerator FadeOnAppearing(bool fadeIn)
-    {
-        if (isFading)
-        {
-            yield break;
-        }
-        isFading = true;
-        float duration = 1;
-        float elapsedTime = 0;
-        gameObject.layer = fadeIn ? originalLayer : 2;
-        foreach (var renderer in meshRenderers)
-        {
-            renderer.enabled = true;
-        }
-
-
-        while (elapsedTime < duration)
-        {
-            elapsedTime += Time.deltaTime;
-            if (fadeIn)
-            {
-                for (int i = 0; i < meshRenderers.Length; i++)
-                {
-                    meshRenderers[i].material.Lerp(transparentMaterial, rendererMaterials[i], elapsedTime / duration);
-                }
-            }
-            else
-            {
-                for (int i = 0; i < meshRenderers.Length; i++)
-                {
-                    meshRenderers[i].material.Lerp(rendererMaterials[i], transparentMaterial, elapsedTime / duration);
-                }
-            }
-            yield return null;
-        }
-
-        if (!fadeIn)
+        if (isBeingSeen)
         {
             foreach (var renderer in meshRenderers)
             {
                 renderer.enabled = true;
+                gameObject.layer = originalLayer;
             }
         }
-        isVisible = fadeIn;
-        isFading = false;
+        else
+        {
+            foreach (var renderer in meshRenderers)
+            {
+                renderer.enabled = false;               
+                gameObject.layer = 2;
+            }
+        }
     }
 }
