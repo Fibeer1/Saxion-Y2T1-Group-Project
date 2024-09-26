2adffdc0779d0b6b5ea78e477ec07137c4dcec8e
JimmevD <72076148+JimmevD@users.noreply.github.com>
2024-09-26 22:33:53 +0200

    Added Tutorial
    It's not perfect, but will do for now

---
+++ Assets/Scripts/UI scripts/TutorialUI.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using TMPro;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+public class TutorialUI : MonoBehaviour
+{
+    private int currentStep;
+    [SerializeField] private RangerBackground rangerCheck;
+    [SerializeField] private TextMeshProUGUI currentTutorialText;
+
+    [SerializeField] private GameObject store, tech, village;
+    private bool hasOpenedStore, hasOpenedTech, hasOpenedVillage;
+   
+    [System.Serializable]
+    public struct TutorialStep
+    {
+        [TextArea(15, 20)]
+        public string currentText;
+        public UnityEvent unityEvents;
+    }
+
+    [SerializeField] private TutorialStep[] steps;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            #region checks
+            if (currentStep == 1)
+            {
+                if (!rangerCheck.ranger)
+                {
+                    return;
+                }
+            }
+
+            if (currentStep == 3)
+            {
+                if (!hasOpenedStore)
+                {
+                    return;
+                }
+
+                if (store.activeInHierarchy)
+                {
+                    return;
+                }
+            }
+
+            if (currentStep == 4)
+            {
+                if (!hasOpenedTech)
+                {
+                    return;
+                }
+
+                if (tech.activeInHierarchy)
+                {
+                    return;
+                }
+            }
+
+            if (currentStep == 5)
+            {
+                if (!hasOpenedVillage)
+                {
+                    return;
+                }
+
+                if (village.activeInHierarchy)
+                {
+                    return;
+                }
+            }
+
+            #endregion checks 
+            //^Lot of checks so you can't skip tutorial
+
+            if (currentStep >= steps.Length - 1)
+            {
+                gameObject.SetActive(false);
+                return;
+            }
+
+            currentStep++;
+
+            currentTutorialText.text = steps[currentStep].currentText;
+            steps[currentStep].unityEvents.Invoke();
+        }
+
+
+        if (store.activeInHierarchy && !hasOpenedStore)
+        {
+            hasOpenedStore = true;
+        }
+
+        if (tech.activeInHierarchy && !hasOpenedTech)
+        {
+            hasOpenedTech = true;
+        }
+
+        if (village.activeInHierarchy && !hasOpenedVillage)
+        {
+            hasOpenedVillage = true;
+        }
+    }
+}
